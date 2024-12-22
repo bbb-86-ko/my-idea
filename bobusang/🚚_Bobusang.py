a8a60eabf84d13fc4f0d8f371d3ca9aab80c20e9
朴輔培 <bbb.86.ko@gmail.com>
2024-12-22 13:19:34 +0900

    first bobusang

---
+++ bobusang/🚚_Bobusang.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Bobusang",
+    page_icon="🚚",
+)
+
+st.write("# Welcome to Bobusang! 🚚")
+
+# Add a selectbox to the sidebar:
+# st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+        #### 🚚 Bobusang:
+        #### Role:
+        - Identifies needs within communities by observing their daily lives and understanding their wants. Provides solutions to these needs by offering a variety of goods and services directly to the people, often in remote or underserved areas.
+        
+        #### Tasks:
+        - **Identify problems/needs:** Observes the lack of certain goods or services in the communities they visit.
+        - **Provide solutions/functions:** Offers relevant products that address these identified needs, often carrying a diverse range of items to cater to different demands.
+    """
+)
