15c52f58cd8643bc7f72e88fb06dad27b3fd08ad
朴輔培 <bbb.86.ko@gmail.com>
2025-01-05 14:29:43 +0900

    archive

---
+++ bobusang/archive/1_🏃🏻_Are_you_eating_healthy_food?.py
@@ -0,0 +1,94 @@
+import streamlit as st
+from PIL import Image
+import io
+import google.generativeai as genai
+import os
+# from notion_client import Client
+import requests
+import json
+# PDF 라이브러리
+from fpdf import FPDF
+
+# get API key from environment variable
+GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
+
+# API 키 설정
+genai.configure(api_key=GEMINI_API_KEY)
+
+# gemini model 설정
+genaiModel = genai.GenerativeModel("gemini-1.5-flash")
+
+# Streamlit 앱 설정
+st.set_page_config(page_title="Are you eating healthy food?", page_icon="🏃🏻")
+
+# 앱 헤더
+st.title("🏃🏻 Are you eating healthy food?")
+
+# 현재 디렉토리 출력 : /mount/src/my-idea
+# st.write(os.getcwd())
+fornt_path = os.path.join(os.getcwd(), "bobusang/fonts/NanumGothic/NanumGothic-Regular.ttf")
+
+# 이미지 업로드
+uploaded_file = st.file_uploader(
+    label="오늘 먹은 음식 사진을 업로드하세요", 
+    type=["png", "jpg", "jpeg"],
+)
+
+if uploaded_file:
+    # 이미지를 표시
+    image = Image.open(uploaded_file)
+    st.image(image, caption="업로드된 차트 이미지", use_container_width=True)
+    
+    # 차트 이미지 처리 단계 (추후 구현 가능)
+    st.write("이미지에서 차트 데이터를 추출하는 중...")
+    
+    # GPT 및 Gemini API 호출
+    with st.spinner("분석을 진행 중입니다..."):
+        # Gemini 요청 (예: dummy response)
+        gemini_response = genaiModel.generate_content([
+            """
+                답변은 markdown 형식과 한국어로 부탁합니다.
+                음식 사진을 분석하여 대략적인 칼로리 정보를 아래 형식으로 알려드립니다.
+                * 요약 
+                * 예상 칼로리(전체) 
+                * 예상 칼로리(상세) 
+                * 잘한것 
+                * 부족한것 
+                * 조언
+                음식 사진이 아니면 "음식 사진이 아닙니다." 라고 답변해주세요.
+            """,
+            image  # 실제론 이미지 자체를 전달하는 게 아닌, 이미지를 분석한 결과 등을 넣어야 할 수도 있음
+        ])
+        gemini_analysis = gemini_response.text
+
+    # 결과 출력
+    st.subheader("🧐 분석 결과")
+    if gemini_analysis:
+        st.markdown(gemini_analysis)
+    else:
+        st.markdown("분석 결과 없음.")
+
+    # PDF 만들기 버튼
+    if gemini_analysis:
+            pdf = FPDF()  
+            pdf.add_font(
+                "NanumGothic-Regular", 
+                "", 
+                fornt_path, 
+                uni=True
+            )
+            pdf.set_font("NanumGothic-Regular", "", 10)
+            pdf.add_page()
+            pdf.multi_cell(0, 10, gemini_analysis)
+            
+            pdf_buffer_bytearray = pdf.output(dest="S")  # bytearray로 반환되는 경우가 있음
+            pdf_buffer = bytes(pdf_buffer_bytearray)     # bytearray → bytes 변환
+            
+            st.download_button(
+                label="PDF 다운로드",
+                data=pdf_buffer,
+                file_name="bobusang-analysis.pdf",
+                mime="application/pdf")
+    else:
+        st.warning("분석 결과가 비어 있습니다.")
+
